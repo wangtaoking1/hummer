4519f99c56ce4254aceafef589ae9a054539f86c
wangtaoking1 <479021795@qq.com>
2015-12-05 15:56:29 +0800

    test ssh login to github

--- restapi/serializers.py
+++ restapi/serializers.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from restapi.models import MyUser
--- restapi/views.py
+++ restapi/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
 
