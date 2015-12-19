e4d9db2c84204acbef5a1606400f838c652f19c0
wangtaoking1 <479021795@qq.com>
2015-12-19 17:26:53 +0800

    edit k8sclient

--- backend/k8sclient.py
+++ backend/k8sclient.py
@@ -1,5 +1,8 @@
 import json
 import requests
+import logging
+
+logger = logging.getLogger('hummer')
 
 
 class KubeClient(object):
@@ -7,5 +10,81 @@ class KubeClient(object):
     Kubernetes simple python client.
     API: http://kubernetes.io/third_party/swagger-ui/
     """
+    _base_url = None
     def __init__(self, base_url):
-        pass
+        self._base_url = self.add_slash(base_url)
+
+    @classmethod
+    def add_slash(self, url):
+        """
+        Promote that the base url ends with '/'.
+        """
+        if url.endswith('/'):
+            return url
+        return url + '/'
+
+    @property
+    def base_url(self):
+        return self._base_url
+
+    def send_request(self, method, path, body=None):
+        """
+        Send requests to k8s server and get the response.
+        Returns a response dict.
+        """
+        url = self._base_url + path
+        kwargs = {}
+        if body:
+            kwargs['data'] = json.dumps(body)
+
+        try:
+            res = getattr(requests, method.lower())(url, **kwargs)
+        except Exception as error:
+            logger.error(error)
+            logger.error(res)
+            return None
+
+        return res.json()
+
+    def list_nodes(self):
+        """
+        List all namespaces.
+        """
+        res = self.send_request('GET', 'nodes')
+        print(res)
+        nodes = []
+        for item in res.get('items'):
+            nodes.append(item['metadata']['name'])
+        return nodes
+
+    def list_namespces(self):
+        """
+        List all namespaces.
+        """
+        res = self.send_request('GET', 'namespaces')
+        namespaces = []
+        for item in res.get('items'):
+            namespaces.append(item['metadata']['name'])
+        return namespaces
+
+    def create_namespace(self, name):
+        """
+        Create namespace called name.
+        """
+        body = {'apiVersion': 'v1',
+                'kind': 'Namespace',
+                'metadata': {}
+                }
+        body['metadata']['name'] = name
+
+        self.send_request('POST', 'namespaces', body=body)
+
+    def delete_namespace(self, name):
+        """
+        Delete namespace called name.
+        """
+        res = self.send_request('DELETE', 'namespaces/{}'.format(name))
+        print(res)
+
+
+
--- tests/backend/test_k8sclient.py
+++ tests/backend/test_k8sclient.py
@@ -1,15 +1,47 @@
-import unittest
+import unittest, json
 
 from backend.k8sclient import KubeClient
 
 
 class KubeClientTestCase(unittest.TestCase):
     def setUp(self):
-        pass
+        self.client = KubeClient("http://192.168.0.10:8080/api/v1/")
 
     def tearDown(self):
         self.client = None
 
-    def test_create_image(self):
-        pass
+    def test_add_slash(self):
+        url = "http://192.168.0.10:8080"
+        self.assertEqual(KubeClient.add_slash(url), "http://192.168.0.10:8080/")
 
+    def test_create_instance(self):
+        url = "http://192.168.0.10:8080"
+        client = KubeClient(url)
+        self.assertEqual(client.base_url, "http://192.168.0.10:8080/")
+
+    def test_send_request(self):
+        res = self.client.send_request("get", "namespaces", labels={'a': 1, 'name': 'wangtao'})
+        self.assertEqual(isinstance(res, dict), True)
+
+    def test_list_namespaces(self):
+        namespaces = self.client.list_namespces()
+        # print(namespaces)
+        self.assertEqual(len(namespaces), 1)
+
+    def test_list_nodes(self):
+
+        nodes = self.client.list_nodes()
+        print(nodes)
+        self.assertEqual(len(nodes), 1)
+
+    def test_create_namespace(self):
+        self.client.create_namespace('abcd')
+        res = self.client.list_namespces()
+        print(res)
+        self.assertEqual(len(res), len(res))
+
+    def test_delete_namespace(self):
+        before = len(self.client.list_namespces())
+        self.client.delete_namespace('abcd')
+        after = len(self.client.list_namespces())
+        self.assertEqual(before, after + 1)
