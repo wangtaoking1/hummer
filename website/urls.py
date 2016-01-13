c5df4e4a0103283ebf9b1b7ac2465e29059eced9
wangtaoking1 <479021795@qq.com>
2016-01-13 20:19:06 +0800

    add static and templates file directories

--- website/urls.py
+++ website/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import include, url
-
+from website import views
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'hummer.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^api/', include('restapi.urls')),
-    url(r'', include('website.urls')),
+    url(r'^$', views.index, name='index'),
 ]
